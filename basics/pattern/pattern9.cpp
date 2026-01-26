12218359c672973ab66e1f8c9598d883cf2d5c1e
aviralumrao <aviral0419@gmail.com>
2026-01-26 19:26:42 +0530

    Diamond Star Pattern

---
+++ basics/pattern/pattern9.cpp
@@ -0,0 +1,43 @@
+/*Pattern - 9: Diamond Star Pattern*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    int i, j, k;
+    for (i = 1; i <= n; i++)
+    {
+        for (k = 1; k <= n-i; k++)
+        {
+            cout << " ";
+        }
+
+        for (j = 1; j< 2*i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+     for (i = n; i >= 1; i--)
+    {
+        for (k = 1; k <= n-i; k++)
+        {
+            cout << " ";
+        }
+
+        for (j = 1; j< 2*i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cout << "Input an Integer N = ";
+    cin >> t;
+    print(t);
+    return 0;
+}

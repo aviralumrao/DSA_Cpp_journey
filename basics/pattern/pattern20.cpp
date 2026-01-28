dd690a24fb5d54939f3f77620a8043d3c94b5c51
aviralumrao <aviral0419@gmail.com>
2026-01-28 21:32:16 +0530

    Symmetric-Void Pattern

---
+++ basics/pattern/pattern20.cpp
@@ -0,0 +1,71 @@
+// Pattern - 19: Symmetric-Void Pattern
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n)
+{
+    // Upper half (excluding middle row)
+    for (int i = 0; i < n; i++)
+    {
+        int spaces = 2 * (n - i - 1);
+
+        // Left stars
+        for (int j = 0; j <= i; j++)
+            cout << "*";
+
+        // Middle spaces
+        for (int j = 0; j < spaces; j++)
+            cout << " ";
+
+        // Right stars
+        for (int j = 0; j <= i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // Lower half (including middle row)
+    for (int i = 1; i < n; i++)
+    {
+        int spaces = 2 * i;
+
+        // Left stars
+        for (int j = 0; j < n - i; j++)
+            cout << "*";
+
+        // Middle spaces
+        for (int j = 0; j < spaces; j++)
+            cout << " ";
+
+        // Right stars
+        for (int j = 0; j < n - i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+}
+
+int main()
+{
+    int n;
+    cout << "Input an Integer N = ";
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
+
+
+/*
+OUTPUT
+Input an Integer N = 5
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/

9e4ef3112ac1135ea99a43bf91518a22c1085cd0
aviralumrao <aviral0419@gmail.com>
2026-03-19 02:35:21 +0530

    add: implement merge sort algorithm

---
+++ 02_sorting/04_merge_sort.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+        arr[i] = temp[i - low];
+}
+
+void mergesort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergesort(arr, low, mid);
+    mergesort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    mergesort(arr, 0, arr.size() - 1);
+
+    for (int x : arr)
+        cout << x << " ";
+    return 0;
+}
